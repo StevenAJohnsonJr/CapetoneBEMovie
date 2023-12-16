33789ddc06da3fe73ae0b35f527836bd4438b4e4
StevenAJohnsonJr <sajohhnson187@gmail.com>
2023-12-16 10:08:09 -0600

    viedoKey

--- Migrations/20231213152521_VideoKey2Create.Designer.cs
+++ Migrations/20231213152521_VideoKey2Create.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CapstoneBEMovie;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CaptoneBEMovie.Migrations
 {
     [DbContext(typeof(CapstoneBEMovieDbContext))]
-    partial class CapstoneBEMovieDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231213152521_VideoKey2Create")]
+    partial class VideoKey2Create
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
